564447e4b3284e0d9f499c0b9a59695c95e42507
Carter <trpik4@gmail.com>
2020-12-09 00:27:00 -0500

    Initial model work.

--- movie_rating/secrets.py
+++
@@ -1,3 +0,0 @@
-""""""
-
-SECRET_KEY_PROD = 'a^ab@v8%86d!=u2c6yz7m&jvsb8tkxaq5lf_q@6^!onew@3vl_'
--- movie_rating/settings.py
+++ movie_rating/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ratings'
 ]
 
 MIDDLEWARE = [
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'movie_rating.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
